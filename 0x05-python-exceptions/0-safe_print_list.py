28191fa8517f98389438170c660f6401bb1be7b4
Rawllings <frawllings@example.com>
2022-12-19 12:32:13 -0800

    message

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,9 +3,9 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     for i in range(x):
         try:
-            print(f"{my_list[i]}", end=" ")
+            print('{:d}'.format(my_list[i]), end=" ")
             printed += 1
         except:
             continue
-        print()
-        return printed
+    print()
+    return printed
