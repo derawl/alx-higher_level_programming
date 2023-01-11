ec55532416c0285e5fe1045a591ecdbd27c8faff
Rawllings <frawllings@example.com>
2023-01-10 23:56:34 -0800

    message

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
-"""
-This module reads a file name
-"""
+"""This module reads a file name"""
+
 
 def read_file(filename=""):
-    """Prints the contents of a file
-    Arguments:
+    """Function that Prints the contents of a file
+    
+    Args:
      filename: name of the file
+     
     Raises:
         Exception: when file can't be opened
+        
     """
+    
     with open(filename) as file:
         read = file.read()
         print(read, end='')
