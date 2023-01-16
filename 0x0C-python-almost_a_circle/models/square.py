f9269459bccf91f37ff52494ffd5c14147d838b1
JesusKid <frawllings@gmail.com>
2023-01-16 10:29:21 -0800

    message

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -32,7 +32,8 @@ class Square(Rectangle):
             raise TypeError("width must be an integer")
         if value <= 0:
             raise ValueError("width must be > 0")
-
+        
+        #sets the value to the same
         self.width = value
         self.height = value
 
