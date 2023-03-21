57da398923f299d27ec45121bea6e7228266f317
Jesuskid <frawllings@gmail.com>
2023-03-21 20:51:04 +0100

    select

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the
+database `hbtn_0e_0_usa`.
+"""
+
 import MySQLdb
 import sys
 
+
+
 if __name__ == '__main__':
+    """
+    Connects to database and retrieves all records for states
+    """
     args = sys.argv
     db = MySQLdb.connect(host='localhost', user=args[1], passwd=args[2], db=args[3], port=3000)
 
