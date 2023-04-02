b9adf8a46754105506b901c5a1ffacbe822ece99
Jesuskid <frawllings@gmail.com>
2023-04-02 08:59:54 +0100

    first commit

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-#echos the length of the response body
-
-url=$1
-
-size=$(curl -sI "url" | grep -i "Content-length" | awk '{print $2}')
-
+size=$(curl -sI "$1" | grep -i "Content-length" | awk '{print $2}')
 echo $size
