289540b540682bbe6967800b34a0c5320ca3a7e7
Jesuskid <frawllings@gmail.com>
2023-04-08 20:24:41 +0100

    6-peak binary search

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,19 @@
+'''
+module finds the peak in a list
+'''
+
+def find_peak(list_of_integers):
+    """
+    Takes a list of integer and returns the highest
+    """
+    low=0
+    high = len(list_of_integers) - 1
+
+    while low < high:
+        mid = int((low + high) / 2)
+
+        if(list_of_integers[mid] > list_of_integers[mid + 1]):
+            high = mid
+        else:
+            low = mid + 1
+    return list_of_integers[low]
