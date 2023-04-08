b6ac94bc4a300f309d49439838c21f9416854d50
Jesuskid <frawllings@gmail.com>
2023-04-08 19:40:55 +0100

    3-methods

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a GET request to the URL, and displays the body of the response
-curl -sL "$1" -X GET -D ./header -o ./output && awk 'BEGIN {code=0} $1=="HTTP/1.1" || $1=="HTTP/2" {code=$2} END {if (code==200) exit 0; else exit 1}' ./header && cat ./output
+curl -sL "$1" -X GET -D ./header -o ./output; if awk 'NR==1 && $2==200 {exit 0} {exit 1}' ./header; then cat ./output; fi
