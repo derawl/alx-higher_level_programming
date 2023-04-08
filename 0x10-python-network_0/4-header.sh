120970e5932596d0de80ce58791298b48109d603
Jesuskid <frawllings@gmail.com>
2023-04-08 20:10:42 +0100

    post params

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Send a GET request to the URL using curl with the custom header and display the response body
-curl -s -X GET -H "X-School-User-Id: 98" "$1"
+curl -sX GET -H "X-School-User-Id:98" "$1"
